0a55c0fc527e9bbb040c3926920e4897cfd06b1a
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2020-11-22 16:34:11 +0200

    Добавлено два файла

---
+++ js/tasks.js
@@ -0,0 +1,16 @@
+import users from './users.js';
+
+// =============================== Задание 1 ===============================
+// const getUserNames = users => users.map(user => user.name);
+
+// console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+// =============================== Задание 2 ===============================
+
+// const getUsersWithEyeColor = (users, color) =>
+// users.filter(({ eyeColor }) => eyeColor === color);
+
+// console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+// =============================== Задание 3 ===============================
