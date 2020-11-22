555a7537183df387f63bc1247969620d38eedb67
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2020-11-22 16:50:02 +0200

    Выполнено пять заданий

--- js/tasks.js
+++ js/tasks.js
@@ -14,3 +14,22 @@ import users from './users.js';
 // console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
 // =============================== Задание 3 ===============================
+
+// const getUsersWithGender = (users, gender) =>
+// users.filter(user => user.gender === gender).map(({ name }) => name);
+
+// console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
+// =============================== Задание 4 ===============================
+
+// const getInactiveUsers = users => users.filter(({ isActive }) => !isActive);
+
+// console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+// =============================== Задание 5 ===============================
+
+const getUserWithEmail = (users, email) =>
+  users.find(user => user.email === email);
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
