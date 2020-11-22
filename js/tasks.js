dc73aa644cdfd060eeecfabd46704b2b8be7b032
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2020-11-22 18:12:47 +0200

    Выполнены все задания

--- js/tasks.js
+++ js/tasks.js
@@ -28,8 +28,56 @@ import users from './users.js';
 
 // =============================== Задание 5 ===============================
 
-const getUserWithEmail = (users, email) =>
-  users.find(user => user.email === email);
+// const getUserWithEmail = (users, email) =>
+// users.find(user => user.email === email);
 
-console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
-console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+// console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+// console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+
+// =============================== Задание 6 ===============================
+
+// const getUsersWithAge = (users, min, max) =>
+// users.filter(({ age }) => age >= min && age <= max);
+
+// console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+// console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+// =============================== Задание 7 ===============================
+
+// const calculateTotalBalance = users =>
+// users.reduce((totalBalance, user) => (totalBalance += user.balance), 0);
+
+// console.log(calculateTotalBalance(users)); // 20916
+
+// =============================== Задание 8 ===============================
+
+// const getUsersWithFriend = (users, friendName) =>
+// users
+// .filter(({ friends }) => friends.includes(friendName))
+// .map(({ name }) => name);
+
+// console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+// console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+
+// =============================== Задание 9 ===============================
+
+// const getNamesSortedByFriendsCount = users =>
+//   users
+//     .sort((a, b) => a.friends.length - b.friends.length)
+//     .map(({ name }) => name);
+
+// console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+// =============================== Задание 10 ===============================
+
+const getSortedUniqueSkills = users =>
+  users
+    .reduce((totalSkills, user) => [...totalSkills, ...user.skills], [])
+    .filter((skill, idx, skills) => skills.indexOf(skill) === idx)
+    .sort();
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
